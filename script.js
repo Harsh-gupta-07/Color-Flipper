a5b67ddacecdd3b770d32a3384bd71afd2ff6ff3
Harshvardhan Gupta <harshvardhangofficial@gmail.com>
2024-11-26 15:37:25 +0530

    Added rgba

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ let header =  document.getElementById("color")
 let colorcode = document.getElementById("Changecolorcode")
 
 function changecol(){
-//    console.log(colorcode.value)
-
    if(colorcode.value == "rgb"){
         colorrgb();
-   }else{
+   }else if(colorcode.value == "rgba"){
+        colorrgba()
+   }
+   else{
         colorhex()
    }
-    // `color${colorcode.value}()`
 }
 
 function randomcolor(){
     return  Math.floor(Math.random()*256)
 }
 
+function randomopacity(){
+    return Math.random().toFixed(2)
+}
+
 function colorrgb(){
+    document.querySelector("div").style.width = "600px"
     let rgb = [randomcolor(),randomcolor(),randomcolor()]
     console.log(rgb)
     header.innerText= `Current Color: rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
@@ -24,6 +29,7 @@ function colorrgb(){
 }
 
 function colorhex(){
+    document.querySelector("div").style.width = "600px"
     let rgb = [converthex(), converthex(),converthex()]
     console.log(rgb)
     document.querySelector("body").style.backgroundColor = `#${rgb[0]}${rgb[1]}${rgb[1]}`
@@ -47,4 +53,13 @@ function converthex(){
         temp= `0${temp}`
     }
     return temp
+}
+
+
+function colorrgba(){
+    document.querySelector("div").style.width = "700px"
+    let rgb = [randomcolor(),randomcolor(),randomcolor(),randomopacity()]
+    console.log(rgb)
+    header.innerText= `Current Color: rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${rgb[3]})`
+    document.querySelector("body").style.backgroundColor = `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${rgb[3]})`
 }
